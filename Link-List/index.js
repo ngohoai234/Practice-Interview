b91a0b8efe77a9a49b61284b1f013e4bee8b1e22
ngohoai234 <cangod05@gmail.com>
2023-04-09 14:43:40 +0700

    Coding remove duplicate value

---
+++ Link-List/index.js
@@ -0,0 +1,82 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkList {
+  head = null;
+
+  constructor(value) {
+    this.head = new Node(value);
+  }
+
+  add(val) {
+    const newNode = new Node(val);
+
+    let currentNode = this.head;
+    if (currentNode) {
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    } else {
+      this.head = newNode;
+    }
+
+    return newNode;
+  }
+
+  delete(val) {
+    let currentNode = this.head;
+    if (!currentNode) {
+      return false;
+    }
+    debugger;
+    if (val === currentNode.val) {
+      this.head = null;
+      return true;
+    } else {
+      while (currentNode.next) {
+        if (currentNode.next.value === val) {
+          currentNode.next = currentNode.next.next;
+          return true;
+        }
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
+  }
+
+  deleteDuplicate() {
+    let currentNode = this.head;
+    if (!currentNode) {
+      return;
+    }
+    while (currentNode) {
+      let runner = currentNode;
+      while (runner.next) {
+        if (runner.next.value === currentNode.value) {
+          runner.next = runner.next.next;
+        } else {
+          runner = runner.next;
+        }
+      }
+      currentNode = currentNode.next;
+    }
+  }
+
+  print() {
+    let currentNode = this.head;
+    while (currentNode) {
+      console.log(currentNode.value);
+      currentNode = currentNode.next;
+    }
+  }
+}
+
+module.exports = {
+  Node,
+  LinkList,
+};
---
+++ Link-List/test.js
@@ -0,0 +1,19 @@
+const { LinkList } = require('./index');
+
+const list = new LinkList(1);
+
+list.add(1);
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(3);
+
+// console.log('Before remove');
+
+// list.print();
+
+// list.deleteDuplicate();
+
+// console.log('After remove');
+
+// list.print();
